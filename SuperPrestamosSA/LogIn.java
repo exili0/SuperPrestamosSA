6c69d86d539e5e2e7cb35be88391a82944502935
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-04-28 19:42:05 +0200

    Update LogIn.java

--- SuperPrestamosSA/LogIn.java
+++ SuperPrestamosSA/LogIn.java
@@ -17,20 +17,20 @@ public class LogIn extends Registro {
 
 		do {
 			System.out.printf("➤");
-			opcionLogin = sc.next(); // INTRODUCIR OPCION DE LOGIN
+			opcionLogin = sc.next(); // INTRODUCIR OPCIÓN DE LOGIN
 
 			if (opcionLogin.equals("1")) {
 				
 				Administrador admin1= new Administrador();
 				admin1.loginAdministrador(nickname, contrasenya); // LLAMA AL METODO QUE ESTÁ EN ADMINISTRADOR	
-				opcionLogin="salir"; //𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄 Salir del programa
+				opcionLogin="salir"; // Salir del programa
 			}
 			
-			else if (opcionLogin.equals("2")) { //𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄 LOS BUCLES DE CONTRASEÑA Y NICKNAME FUNCIONAN IGUAL QUE ANTES
+			else if (opcionLogin.equals("2")) { // LOS BUCLES DE CONTRASEÑA Y NICKNAME FUNCIONAN IGUAL QUE ANTES
 				
 				SuperEmpresa empresa1= new SuperEmpresa();
 				empresa1.loginSuperEmpresa(nickname, contrasenya); // LLAMA AL METODO QUE ESTÁ EN SUPEREMPRESA
-				opcionLogin="salir";//𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄 Salir del programa
+				opcionLogin="salir";// Salir del programa
 			} 
 			else {
 				System.out.print("𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄 Opción no válida 𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄\n");
@@ -41,30 +41,33 @@ public class LogIn extends Registro {
 		//	Por lo que sea (!opcionLogin.equals("1") || !opcionLogin.equals("1")) no funciona
 		// 	Es por ello que saldrá cuando se detecte el valor "salir"
 		
-		System.out.println("𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄 [ TE HAS REGISTRADO CORRECTAMENTE ] 𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄");
+		System.out.println("𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄 [ TE HAS LOGUEADO CORRECTAMENTE ] 𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄𝙄");
 	}
 
 	public boolean verificarContrasenya(String contrasenya) {
 		boolean contieneMayus = false;
 		boolean contieneMinus = false;
 		boolean contieneNum = false;
-		boolean valida;
+		boolean valida = false;
 		
-		for (int i = 0; i < contrasenya.length(); i++) {
-			char caracter = contrasenya.charAt(i);
-			if (Character.isUpperCase(caracter)) {
-				contieneMayus = true;
-			} else if (Character.isLowerCase(caracter)) {
-				contieneMinus = true;
-			} else if (Character.isDigit(caracter)) {
-				contieneNum = true;
+		if(contrasenya.length() >= 6) { // Debe contener al menos 6 caracteres
+			for (int i = 0; i < contrasenya.length(); i++) {
+				char caracter = contrasenya.charAt(i);
+				if (Character.isUpperCase(caracter)) {
+					contieneMayus = true;
+				} else if (Character.isLowerCase(caracter)) {
+					contieneMinus = true;
+				} else if (Character.isDigit(caracter)) {
+					contieneNum = true;
+				}
 			}
+			
+			valida = (contieneMayus && contieneMinus && contieneNum); // Solo será true si las 3 lo son
 		}
+
 		
-		valida = (contieneMayus && contieneMinus && contieneNum); // Solo será true si las 3 lo son
-		
-		if(valida == false) {
-			System.out.println("Contraseña incorrecta ✖\n \n[ La contraseña debe contener al menos un número, una mayúscula y una minúscula ]\n");
+		else if(valida == false) {
+			System.out.println("Contraseña incorrecta ✖\n \n[ La contraseña debe contener al menos 6 caracteres, un número, una mayúscula y una minúscula ]\n");
 		}
 		else {
 			System.out.println("Contraseña validada ✔");
@@ -73,19 +76,18 @@ public class LogIn extends Registro {
 	}
 	
 	public boolean verificarNickname(String nickname) {
-		boolean valida = true;
+		boolean valido = true;
 		
 		for (int i = 0; i < nickname.length(); i++) {
 			char caracter = nickname.charAt(i);
 			
 			if (caracter >= 128 && caracter <= 255){ // Este rango en ASCII es el de los cáracteres que no son anglosajones
-				valida = false;
-				break;
+				valido = false;
 			}
 			else {
-				valida = true;
+				valido = true;
 			}
 		}
-		return valida;
+		return valido;
 	}
 }
