19a18b614682a1b422a661847c6e8d6aadddda62
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-13 21:20:19 +0200

    Create ModeloImp.java

---
+++ SuperPrestamosSA/ModeloImp.java
@@ -0,0 +1,17 @@
+package SuperPrestamosSA;
+
+public class ModeloImp implements Modelo{
+
+	private Vista vista;
+	
+	public ModeloImp() {
+		super();
+	}
+
+	public void setVista(Vista vista) {
+		this.vista = vista;
+		
+	}
+	
+	
+}
