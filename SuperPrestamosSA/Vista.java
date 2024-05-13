d17745f67d0c42f465001cd2dae7635d0429ddab
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-13 21:20:58 +0200

    Create Vista.java

---
+++ SuperPrestamosSA/Vista.java
@@ -0,0 +1,11 @@
+package SuperPrestamosSA;
+
+import java.awt.Container;
+
+public interface Vista {
+
+	public void setControlador(Controlador controlador);
+	public void setModelo(Modelo modelo);
+	public Container getContenedor();
+
+}
