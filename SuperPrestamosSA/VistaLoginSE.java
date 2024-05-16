0bc9077e70ac701f39b84dedd030766ea87f3d45
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-16 21:32:42 +0200

    Update VistaLoginSE.java

--- SuperPrestamosSA/VistaLoginSE.java
+++ SuperPrestamosSA/VistaLoginSE.java
@@ -1,7 +1,10 @@
 package SuperPrestamosSA;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,7 +18,7 @@ public class VistaLoginSE extends JFrame implements Vista {
 		pack();
 		setTitle("Página login SuperEmpresa");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 690, 329);
+		setBounds(100, 100, 800, 600);
 		getContentPane().setLayout(null);
 		setVisible(true);
 
@@ -23,6 +26,19 @@ public class VistaLoginSE extends JFrame implements Vista {
 		lblTitulo.setBounds(215, 10, 380, 136);
 		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		getContentPane().add(lblTitulo);
+		
+		JButton btnEntrarSE = new JButton("INICIAR SESIÓN");
+		btnEntrarSE.setBackground(new Color(255, 255, 255));
+		btnEntrarSE.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnEntrarSE.setBounds(216, 210, 343, 47);
+		getContentPane().add(btnEntrarSE);
+		btnEntrarSE.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				VistaRecursos vistaRecursos = new VistaRecursos();
+				vistaRecursos.setVisible(true);
+			}
+		});
 	}
 
 	@Override
