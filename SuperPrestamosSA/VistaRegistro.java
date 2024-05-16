b51a4fb7cbde842745c72ee8f37f5a91b52f0f1d
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-16 20:52:50 +0200

    Update VistaRegistro.java

--- SuperPrestamosSA/VistaRegistro.java
+++ SuperPrestamosSA/VistaRegistro.java
@@ -5,6 +5,9 @@ import java.awt.Container;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import java.awt.Color;
 
@@ -15,7 +18,7 @@ public class VistaRegistro extends JFrame implements Vista{
 		pack();
 		setTitle("Página registro");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 679, 532);
+		setBounds(100, 100, 800, 600);
 		getContentPane().setLayout(null);
 		setVisible(true);
 
@@ -29,12 +32,26 @@ public class VistaRegistro extends JFrame implements Vista{
 		btnRegistroAdmin.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnRegistroAdmin.setBounds(216, 210, 343, 47);
 		getContentPane().add(btnRegistroAdmin);
+		btnRegistroAdmin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				VistaRegistroAdmin vistaRegistroAdmin = new VistaRegistroAdmin();
+				vistaRegistroAdmin.setVisible(true);
+			}
+		});
 		
-		JButton btnRegistroSuperE = new JButton("REGISTRARSE COMO SUPEREMPRESA");
-		btnRegistroSuperE.setBackground(new Color(255, 255, 255));
-		btnRegistroSuperE.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnRegistroSuperE.setBounds(216, 315, 343, 47);
-		getContentPane().add(btnRegistroSuperE);
+		JButton btnRegistroSE = new JButton("REGISTRARSE COMO SUPEREMPRESA");
+		btnRegistroSE.setBackground(new Color(255, 255, 255));
+		btnRegistroSE.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnRegistroSE.setBounds(216, 315, 343, 47);
+		getContentPane().add(btnRegistroSE);
+		btnRegistroSE.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				VistaRegistroSE vistaRegistroSE = new VistaRegistroSE();
+				vistaRegistroSE.setVisible(true);
+			}
+		});
 	}
 
 	@Override
