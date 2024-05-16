3cf20b4b08021b8ee117ae3c8e981a0a5c9b93da
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-16 21:33:58 +0200

    Update VistaRegistroSE.java

--- SuperPrestamosSA/VistaRegistroSE.java
+++ SuperPrestamosSA/VistaRegistroSE.java
@@ -1,8 +1,12 @@
 package SuperPrestamosSA;
 
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -22,6 +26,19 @@ public class VistaRegistroSE extends JFrame implements Vista {
 		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		lblTitulo.setBounds(215, 10, 380, 136);
 		getContentPane().add(lblTitulo);
+		
+		JButton btnAltaSE = new JButton("REGISTRARSE");
+		btnAltaSE.setBackground(new Color(255, 255, 255));
+		btnAltaSE.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnAltaSE.setBounds(216, 210, 343, 47);
+		getContentPane().add(btnAltaSE);
+		btnAltaSE.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				VistaImp vistaImp = new VistaImp();
+				vistaImp.setVisible(true);
+			}
+		});
 	}
 
 	@Override
