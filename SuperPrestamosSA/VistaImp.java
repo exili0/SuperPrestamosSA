6b8c725861bf6afd38bb21e2c084444818ae7b76
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-16 20:51:46 +0200

    Update VistaImp.java

--- SuperPrestamosSA/VistaImp.java
+++ SuperPrestamosSA/VistaImp.java
@@ -19,25 +19,35 @@ public class VistaImp extends JFrame implements Vista {
 		pack();
 		setTitle("Página principal");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 733, 611);
-		getContentPane().setLayout(null);
+		setBounds(100, 100, 800, 600);
 		setVisible(true);
+		getContentPane().setLayout(null);
 
 		JLabel lblTitulo = new JLabel("SUPER PRÉSTAMOS S.A.");
-		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		lblTitulo.setBounds(215, 10, 380, 136);
+		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		getContentPane().add(lblTitulo);
 
 		JButton btnLogin = new JButton("LOG IN");
 		btnLogin.setBounds(276, 203, 170, 44);
 		getContentPane().add(btnLogin);
-
-		JButton btnRegistro = new JButton("REGISTRO");
-		btnRegistro.addActionListener(new ActionListener() {
+		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				VistaLogin vistaLogin = new VistaLogin();
+				vistaLogin.setVisible(true);
 			}
 		});
+
+		JButton btnRegistro = new JButton("REGISTRO");
 		btnRegistro.setBounds(276, 299, 170, 44);
+		btnRegistro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				VistaRegistro vistaRegistro = new VistaRegistro();
+				vistaRegistro.setVisible(true);
+			}
+		});
 		getContentPane().add(btnRegistro);
 
 		JButton btnFaq = new JButton("PREGUNTAS FRECUENTES");
