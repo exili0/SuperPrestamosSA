3f1dc7a86860e9f2f97a9381418ef60b6241bb7a
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-17 12:42:01 +0200

    Update VistaRecursos.java

--- SuperPrestamosSA/VistaRecursos.java
+++ SuperPrestamosSA/VistaRecursos.java
@@ -6,38 +6,49 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VistaRecursos extends JFrame implements Vista{
 	
 	public VistaRecursos() {
 
-		pack();
-		setTitle("Página recursos");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 868, 605);
+		JFrame frame5 = new JFrame("Ventana Recursos");
+		frame5.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame5.setBounds(100, 100, 868, 605);
 		getContentPane().setLayout(null);
-		setVisible(true);
+		frame5.getContentPane().setLayout(null);
 
 		JLabel lblTitulo = new JLabel("RECURSOS");
 		lblTitulo.setBounds(330, 10, 380, 136);
 		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		getContentPane().add(lblTitulo);
+		frame5.getContentPane().add(lblTitulo);
 		
 		JButton btnTomarRecurso = new JButton("TOMAR RECURSO");
-		btnTomarRecurso.setBounds(320, 170, 200, 50);
-		getContentPane().add(btnTomarRecurso);
+		btnTomarRecurso.setBounds(300, 156, 200, 50);
+		frame5.getContentPane().add(btnTomarRecurso);
 		
 		JButton btnDejarRecurso = new JButton("DEJAR RECURSO");
-		btnDejarRecurso.setBounds(320, 240, 200, 50);
-		getContentPane().add(btnDejarRecurso);
+		btnDejarRecurso.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnDejarRecurso.setBounds(300, 236, 200, 50);
+		frame5.getContentPane().add(btnDejarRecurso);
 		
 		JButton btnRecursosDisp = new JButton("VER RECURSOS DISPONIBLES");
-		btnRecursosDisp.setBounds(280, 310, 300, 50);
-		getContentPane().add(btnRecursosDisp);
+		btnRecursosDisp.setBounds(252, 408, 300, 50);
+		frame5.getContentPane().add(btnRecursosDisp);
 		
 		JButton btnRegistroRecurso = new JButton("REGISTRAR RECURSO");
-		btnRegistroRecurso.setBounds(320, 380, 200, 50);
-		getContentPane().add(btnRegistroRecurso);
+		btnRegistroRecurso.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnRegistroRecurso.setBounds(300, 312, 200, 50);
+		frame5.getContentPane().add(btnRegistroRecurso);
+		
+		frame5.setVisible(true);
 	}
 
 	@Override
