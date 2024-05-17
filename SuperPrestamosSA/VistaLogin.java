abffe65ec3255e83c2300674f087e4d4baef6f4a
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-17 12:41:14 +0200

    Update VistaLogin.java

--- SuperPrestamosSA/VistaLogin.java
+++ SuperPrestamosSA/VistaLogin.java
@@ -13,46 +13,46 @@ import java.awt.Color;
 
 public class VistaLogin extends JFrame implements Vista{
 	public VistaLogin() {
-		getContentPane().setLayout(null);
 		
-		pack();
-		setTitle("Página log in");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
-		getContentPane().setLayout(null);
-		setVisible(true);
+		JFrame frame2 = new JFrame("Ventana Login");
+		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame2.setBounds(100, 100, 800, 600);
+		frame2.getContentPane().setLayout(null);
 
 		JLabel lblTitulo = new JLabel("LOG IN");
 		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		lblTitulo.setBounds(353, 10, 380, 136);
-		getContentPane().add(lblTitulo);
+		frame2.getContentPane().add(lblTitulo);
 		
 		JButton btnLoginAdmin = new JButton("INICIAR SESIÓN COMO ADMINISTRADOR");
 		btnLoginAdmin.setBackground(new Color(255, 255, 255));
 		btnLoginAdmin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnLoginAdmin.setBounds(216, 210, 343, 47);
-		getContentPane().add(btnLoginAdmin);
+		btnLoginAdmin.setBounds(242, 189, 343, 47);
+		frame2.getContentPane().add(btnLoginAdmin);
 		btnLoginAdmin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
+				frame2.setVisible(false);
 				VistaLoginAdmin vistaLoginAdmin = new VistaLoginAdmin();
-				vistaLoginAdmin.setVisible(true);
+//				vistaLoginAdmin.setVisible(true);
 			}
 		});
 		
 		JButton btnLoginSE = new JButton("INICIAR SESIÓN COMO SUPEREMPRESA");
 		btnLoginSE.setBackground(new Color(255, 255, 255));
 		btnLoginSE.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnLoginSE.setBounds(216, 315, 343, 47);
-		getContentPane().add(btnLoginSE);
+		btnLoginSE.setBounds(242, 311, 343, 47);
+		frame2.getContentPane().add(btnLoginSE);
 		btnLoginSE.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
+				frame2.setVisible(false);
 				VistaLoginSE vistaLoginSE = new VistaLoginSE();
-				vistaLoginSE.setVisible(true);
+//				vistaLoginSE.setVisible(true);
 			}
 		});
+		
+		frame2.setVisible(true);
 	}
+	
 
 	@Override
 	public void setControlador(Controlador controlador) {
