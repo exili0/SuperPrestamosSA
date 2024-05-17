93292b0a37fadd9d72256d3dea603bb208057e8c
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-17 14:19:10 +0200

    Update VistaRecursos.java

--- SuperPrestamosSA/VistaRecursos.java
+++ SuperPrestamosSA/VistaRecursos.java
@@ -1,83 +1,94 @@
 package SuperPrestamosSA;
 
+import java.awt.Color;
 import java.awt.Container;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class VistaRegistro extends JFrame implements Vista{
-	public VistaRegistro() {
-		
-		JFrame frame6 = new JFrame("Ventana Registro");
-		frame6.getContentPane().setBackground(new Color(151, 192, 109));
-		frame6.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame6.setBounds(100, 100, 800, 600);
-		frame6.getContentPane().setLayout(null);
+public class VistaRecursos extends JFrame implements Vista{
+	
+	public VistaRecursos() {
 
-		JLabel lblTitulo = new JLabel("REGISTRO");
+		JFrame frame5 = new JFrame("Ventana Recursos");
+		frame5.getContentPane().setBackground(new Color(151, 192, 109));
+		frame5.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame5.setBounds(100, 100, 868, 605);
+		getContentPane().setLayout(null);
+		frame5.getContentPane().setLayout(null);
+
+		JLabel lblTitulo = new JLabel("RECURSOS");
+		lblTitulo.setBounds(317, 40, 193, 106);
 		lblTitulo.setFont(new Font("Segoe Script", Font.PLAIN, 30));
-		lblTitulo.setBounds(300, 51, 185, 97);
-		frame6.getContentPane().add(lblTitulo);
-
-		JButton btnRegistroAdmin = new JButton("REGISTRARSE COMO ADMINISTRADOR");
-		btnRegistroAdmin.setBackground(new Color(204, 231, 197));
-		btnRegistroAdmin.setFont(new Font("Segoe Script", Font.PLAIN, 15));
-		btnRegistroAdmin.setBounds(195, 224, 400, 47);
-		frame6.getContentPane().add(btnRegistroAdmin);
-		btnRegistroAdmin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame6.setVisible(false);
-				VistaRegistroAdmin vistaRegistroAdmin = new VistaRegistroAdmin();
-//				vistaRegistroAdmin.setVisible(true);
-			}
-		});
+		frame5.getContentPane().add(lblTitulo);
 		
-		JButton btnRegistroSE = new JButton("REGISTRARSE COMO SUPEREMPRESA");
-		btnRegistroSE.setBackground(new Color(204, 231, 197));
-		btnRegistroSE.setFont(new Font("Segoe Script", Font.PLAIN, 15));
-		btnRegistroSE.setBounds(195, 331, 400, 47);
-		frame6.getContentPane().add(btnRegistroSE);
-		btnRegistroSE.addActionListener(new ActionListener() {
+		JButton btnTomarRecurso = new JButton("TOMAR RECURSO");
+		btnTomarRecurso.setBackground(new Color(204, 231, 197));
+		btnTomarRecurso.setFont(new Font("Segoe Script", Font.PLAIN, 13));
+		btnTomarRecurso.setBounds(310, 168, 200, 50);
+		frame5.getContentPane().add(btnTomarRecurso);
+		
+		JButton btnDejarRecurso = new JButton("DEJAR RECURSO");
+		btnDejarRecurso.setFont(new Font("Segoe Script", Font.PLAIN, 13));
+		btnDejarRecurso.setBackground(new Color(204, 231, 197));
+		btnDejarRecurso.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				frame6.setVisible(false);
-				VistaRegistroSE vistaRegistroSE = new VistaRegistroSE();
-//				vistaRegistroSE.setVisible(true);
 			}
 		});
+		btnDejarRecurso.setBounds(310, 252, 200, 50);
+		frame5.getContentPane().add(btnDejarRecurso);
+		
+		JButton btnRecursosDisp = new JButton("VER RECURSOS DISPONIBLES");
+		btnRecursosDisp.setBackground(new Color(204, 231, 197));
+		btnRecursosDisp.setFont(new Font("Segoe Script", Font.PLAIN, 13));
+		btnRecursosDisp.setBounds(277, 431, 264, 50);
+		frame5.getContentPane().add(btnRecursosDisp);
+		
+		JButton btnRegistroRecurso = new JButton("REGISTRAR RECURSO");
+		btnRegistroRecurso.setBackground(new Color(204, 231, 197));
+		btnRegistroRecurso.setFont(new Font("Segoe Script", Font.PLAIN, 13));
+		btnRegistroRecurso.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnRegistroRecurso.setBounds(310, 333, 200, 50);
+		frame5.getContentPane().add(btnRegistroRecurso);
 		
 		JButton btnVolver = new JButton("Volver");
 		btnVolver.setFont(new Font("Segoe Print", Font.PLAIN, 13));
 		btnVolver.setBackground(new Color(204, 231, 197));
 		btnVolver.setBounds(28, 30, 100, 29);
-		frame6.getContentPane().add(btnVolver);
+		frame5.getContentPane().add(btnVolver);
 		btnVolver.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				frame6.setVisible(false);
-				VistaImp vistaImp = new VistaImp();
+				frame5.setVisible(false);
+				VistaLoginAdmin vistaLoginAdmin = new VistaLoginAdmin();
 			}
 		});
 		
-		frame6.setVisible(true);
+		frame5.setVisible(true);
 	}
 
 	@Override
 	public void setControlador(Controlador controlador) {
+		
 		
 	}
 
 	@Override
 	public void setModelo(Modelo modelo) {
 		
+		
 	}
 
 	@Override
 	public Container getContenedor() {
+		
 		return null;
-	} 
+	}
+
 }
