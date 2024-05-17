87d5516ae92da78315d6bdc9fbbbec87ccc49e8c
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-17 14:19:28 +0200

    Update VistaRegistro.java

--- SuperPrestamosSA/VistaRegistro.java
+++ SuperPrestamosSA/VistaRegistro.java
@@ -15,19 +15,20 @@ public class VistaRegistro extends JFrame implements Vista{
 	public VistaRegistro() {
 		
 		JFrame frame6 = new JFrame("Ventana Registro");
+		frame6.getContentPane().setBackground(new Color(151, 192, 109));
 		frame6.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame6.setBounds(100, 100, 800, 600);
 		frame6.getContentPane().setLayout(null);
 
 		JLabel lblTitulo = new JLabel("REGISTRO");
-		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		lblTitulo.setBounds(309, 41, 183, 122);
+		lblTitulo.setFont(new Font("Segoe Script", Font.PLAIN, 30));
+		lblTitulo.setBounds(300, 51, 185, 97);
 		frame6.getContentPane().add(lblTitulo);
 
 		JButton btnRegistroAdmin = new JButton("REGISTRARSE COMO ADMINISTRADOR");
-		btnRegistroAdmin.setBackground(new Color(255, 255, 255));
-		btnRegistroAdmin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnRegistroAdmin.setBounds(216, 224, 343, 47);
+		btnRegistroAdmin.setBackground(new Color(204, 231, 197));
+		btnRegistroAdmin.setFont(new Font("Segoe Script", Font.PLAIN, 15));
+		btnRegistroAdmin.setBounds(195, 224, 400, 47);
 		frame6.getContentPane().add(btnRegistroAdmin);
 		btnRegistroAdmin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -38,15 +39,27 @@ public class VistaRegistro extends JFrame implements Vista{
 		});
 		
 		JButton btnRegistroSE = new JButton("REGISTRARSE COMO SUPEREMPRESA");
-		btnRegistroSE.setBackground(new Color(255, 255, 255));
-		btnRegistroSE.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnRegistroSE.setBounds(216, 331, 343, 47);
+		btnRegistroSE.setBackground(new Color(204, 231, 197));
+		btnRegistroSE.setFont(new Font("Segoe Script", Font.PLAIN, 15));
+		btnRegistroSE.setBounds(195, 331, 400, 47);
 		frame6.getContentPane().add(btnRegistroSE);
 		btnRegistroSE.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame6.setVisible(false);
 				VistaRegistroSE vistaRegistroSE = new VistaRegistroSE();
 //				vistaRegistroSE.setVisible(true);
+			}
+		});
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.setFont(new Font("Segoe Print", Font.PLAIN, 13));
+		btnVolver.setBackground(new Color(204, 231, 197));
+		btnVolver.setBounds(28, 30, 100, 29);
+		frame6.getContentPane().add(btnVolver);
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame6.setVisible(false);
+				VistaImp vistaImp = new VistaImp();
 			}
 		});
 		
