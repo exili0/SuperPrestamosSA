c17e127f2102fe0587e24589c90a358ade70921b
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-17 12:42:16 +0200

    Update VistaRegistro.java

--- SuperPrestamosSA/VistaRegistro.java
+++ SuperPrestamosSA/VistaRegistro.java
@@ -13,45 +13,44 @@ import java.awt.Color;
 
 public class VistaRegistro extends JFrame implements Vista{
 	public VistaRegistro() {
-		getContentPane().setLayout(null);
 		
-		pack();
-		setTitle("Página registro");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
-		getContentPane().setLayout(null);
-		setVisible(true);
+		JFrame frame6 = new JFrame("Ventana Registro");
+		frame6.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame6.setBounds(100, 100, 800, 600);
+		frame6.getContentPane().setLayout(null);
 
 		JLabel lblTitulo = new JLabel("REGISTRO");
 		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		lblTitulo.setBounds(330, 10, 380, 136);
-		getContentPane().add(lblTitulo);
+		lblTitulo.setBounds(309, 41, 183, 122);
+		frame6.getContentPane().add(lblTitulo);
 
 		JButton btnRegistroAdmin = new JButton("REGISTRARSE COMO ADMINISTRADOR");
 		btnRegistroAdmin.setBackground(new Color(255, 255, 255));
 		btnRegistroAdmin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnRegistroAdmin.setBounds(216, 210, 343, 47);
-		getContentPane().add(btnRegistroAdmin);
+		btnRegistroAdmin.setBounds(216, 224, 343, 47);
+		frame6.getContentPane().add(btnRegistroAdmin);
 		btnRegistroAdmin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
+				frame6.setVisible(false);
 				VistaRegistroAdmin vistaRegistroAdmin = new VistaRegistroAdmin();
-				vistaRegistroAdmin.setVisible(true);
+//				vistaRegistroAdmin.setVisible(true);
 			}
 		});
 		
 		JButton btnRegistroSE = new JButton("REGISTRARSE COMO SUPEREMPRESA");
 		btnRegistroSE.setBackground(new Color(255, 255, 255));
 		btnRegistroSE.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnRegistroSE.setBounds(216, 315, 343, 47);
-		getContentPane().add(btnRegistroSE);
+		btnRegistroSE.setBounds(216, 331, 343, 47);
+		frame6.getContentPane().add(btnRegistroSE);
 		btnRegistroSE.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
+				frame6.setVisible(false);
 				VistaRegistroSE vistaRegistroSE = new VistaRegistroSE();
-				vistaRegistroSE.setVisible(true);
+//				vistaRegistroSE.setVisible(true);
 			}
 		});
+		
+		frame6.setVisible(true);
 	}
 
 	@Override
