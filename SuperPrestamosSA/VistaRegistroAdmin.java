41d5b30e090c41f60136820a915225b0772616b0
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-17 12:42:31 +0200

    Update VistaRegistroAdmin.java

--- SuperPrestamosSA/VistaRegistroAdmin.java
+++ SuperPrestamosSA/VistaRegistroAdmin.java
@@ -8,37 +8,36 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.FlowLayout;
 import javax.swing.JButton;
 
 public class VistaRegistroAdmin extends JFrame implements Vista {
 
 	public VistaRegistroAdmin() {
 
-		pack();
-		setTitle("Página registro administrador");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
-		getContentPane().setLayout(null);
-		setVisible(true);
+		JFrame frame7 = new JFrame("Ventana Registro Admin");
+		frame7.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame7.setBounds(100, 100, 800, 600);
+		frame7.getContentPane().setLayout(null);
 
 		JLabel lblTitulo = new JLabel("REGISTRO ADMINISTRADOR");
-		lblTitulo.setBounds(220, 10, 500, 136);
+		lblTitulo.setBounds(226, 28, 500, 136);
 		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		getContentPane().add(lblTitulo);
+		frame7.getContentPane().add(lblTitulo);
 		
 		JButton btnAltaAdmin = new JButton("REGISTRARSE");
 		btnAltaAdmin.setBackground(new Color(255, 255, 255));
 		btnAltaAdmin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnAltaAdmin.setBounds(216, 210, 343, 47);
-		getContentPane().add(btnAltaAdmin);
+		btnAltaAdmin.setBounds(308, 298, 210, 58);
+		frame7.getContentPane().add(btnAltaAdmin);
 		btnAltaAdmin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
+				frame7.setVisible(false);
 				VistaImp vistaImp = new VistaImp();
-				vistaImp.setVisible(true);
+//				vistaImp.setVisible(true);
 			}
 		});
+		
+		frame7.setVisible(true);
 	}
 
 	@Override
