2557b6cd41d3f5d3267775d562497af22b91f550
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-17 14:18:05 +0200

    Update VistaLoginAdmin.java

--- SuperPrestamosSA/VistaLoginAdmin.java
+++ SuperPrestamosSA/VistaLoginAdmin.java
@@ -11,32 +11,67 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.Rectangle;
 import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 
 public class VistaLoginAdmin extends JFrame implements Vista {
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	public VistaLoginAdmin() {
 
 		JFrame frame3 = new JFrame("Ventana Login admin");
+		frame3.getContentPane().setBackground(new Color(151, 192, 109));
 		frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame3.setBounds(100, 100, 815, 635);
 		frame3.getContentPane().setLayout(null);
 		
 
 		JLabel lblTitulo = new JLabel("LOGIN ADMINISTRADOR");
-		lblTitulo.setBounds(215, 10, 380, 136);
-		lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblTitulo.setBounds(190, 37, 414, 81);
+		lblTitulo.setFont(new Font("Segoe Script", Font.PLAIN, 30));
 		frame3.getContentPane().add(lblTitulo);
 		
 		JButton btnEntrarAdmin = new JButton("INICIAR SESIÓN");
-		btnEntrarAdmin.setBackground(new Color(255, 255, 255));
-		btnEntrarAdmin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		btnEntrarAdmin.setBounds(283, 254, 189, 49);
+		btnEntrarAdmin.setBackground(new Color(204, 231, 197));
+		btnEntrarAdmin.setFont(new Font("Segoe Script", Font.PLAIN, 15));
+		btnEntrarAdmin.setBounds(270, 422, 224, 47);
 		frame3.getContentPane().add(btnEntrarAdmin);
+		
+		JLabel lblNicknameA = new JLabel("Nickname");
+		lblNicknameA.setFont(new Font("Segoe Script", Font.PLAIN, 20));
+		lblNicknameA.setBounds(314, 158, 112, 36);
+		frame3.getContentPane().add(lblNicknameA);
+		
+		JLabel lblPasswordA = new JLabel("Contraseña");
+		lblPasswordA.setFont(new Font("Segoe Script", Font.PLAIN, 20));
+		lblPasswordA.setBounds(314, 283, 124, 36);
+		frame3.getContentPane().add(lblPasswordA);
+		
+		textField = new JTextField();
+		textField.setBounds(314, 217, 145, 29);
+		frame3.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(314, 333, 145, 29);
+		frame3.getContentPane().add(passwordField);
 		btnEntrarAdmin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame3.setVisible(false);
 				VistaRecursos vistaRecursos = new VistaRecursos();
 //				vistaRecursos.setVisible(true);
+			}
+		});
+		
+		JButton btnVolver = new JButton("Volver");
+		btnVolver.setFont(new Font("Segoe Print", Font.PLAIN, 13));
+		btnVolver.setBackground(new Color(204, 231, 197));
+		btnVolver.setBounds(28, 30, 100, 29);
+		frame3.getContentPane().add(btnVolver);
+		btnVolver.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame3.setVisible(false);
+				VistaLogin vistaLogin = new VistaLogin();
 			}
 		});
 		
