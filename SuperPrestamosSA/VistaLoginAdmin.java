ad4c5db4d4383cdbfdca59ea577d1554688959e4
MarioGonzalezVela <148204798+MarioGonzalezVela@users.noreply.github.com>
2024-05-22 22:14:01 +0200

    Update VistaLoginAdmin.java

--- SuperPrestamosSA/VistaLoginAdmin.java
+++ SuperPrestamosSA/VistaLoginAdmin.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.Rectangle;
 import javax.swing.JTextField;
@@ -24,45 +25,31 @@ public class VistaLoginAdmin extends JFrame implements Vista {
 		frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame3.setBounds(100, 100, 815, 635);
 		frame3.getContentPane().setLayout(null);
-		
 
 		JLabel lblTitulo = new JLabel("LOGIN ADMINISTRADOR");
 		lblTitulo.setBounds(190, 37, 414, 81);
 		lblTitulo.setFont(new Font("Segoe Script", Font.PLAIN, 30));
 		frame3.getContentPane().add(lblTitulo);
-		
-		JButton btnEntrarAdmin = new JButton("INICIAR SESIÓN");
-		btnEntrarAdmin.setBackground(new Color(204, 231, 197));
-		btnEntrarAdmin.setFont(new Font("Segoe Script", Font.PLAIN, 15));
-		btnEntrarAdmin.setBounds(270, 422, 224, 47);
-		frame3.getContentPane().add(btnEntrarAdmin);
-		
+
 		JLabel lblNicknameA = new JLabel("Nickname");
 		lblNicknameA.setFont(new Font("Segoe Script", Font.PLAIN, 20));
 		lblNicknameA.setBounds(314, 158, 112, 36);
 		frame3.getContentPane().add(lblNicknameA);
-		
+
 		JLabel lblPasswordA = new JLabel("Contraseña");
 		lblPasswordA.setFont(new Font("Segoe Script", Font.PLAIN, 20));
 		lblPasswordA.setBounds(314, 283, 124, 36);
 		frame3.getContentPane().add(lblPasswordA);
-		
+
 		textField = new JTextField();
 		textField.setBounds(314, 217, 145, 29);
 		frame3.getContentPane().add(textField);
 		textField.setColumns(10);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setBounds(314, 333, 145, 29);
 		frame3.getContentPane().add(passwordField);
-		btnEntrarAdmin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame3.setVisible(false);
-				VistaRecursos vistaRecursos = new VistaRecursos();
-//				vistaRecursos.setVisible(true);
-			}
-		});
-		
+
 		JButton btnVolver = new JButton("Volver");
 		btnVolver.setFont(new Font("Segoe Print", Font.PLAIN, 13));
 		btnVolver.setBackground(new Color(204, 231, 197));
@@ -74,7 +61,36 @@ public class VistaLoginAdmin extends JFrame implements Vista {
 				VistaLogin vistaLogin = new VistaLogin();
 			}
 		});
-		
+
+		JButton btnEntrarAdmin = new JButton("INICIAR SESIÓN");
+		btnEntrarAdmin.setBackground(new Color(204, 231, 197));
+		btnEntrarAdmin.setFont(new Font("Segoe Script", Font.PLAIN, 15));
+		btnEntrarAdmin.setBounds(270, 422, 224, 47);
+		frame3.getContentPane().add(btnEntrarAdmin);
+		btnEntrarAdmin.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+
+				VistaRegistroAdmin vistaRegistroAdmin = new VistaRegistroAdmin();
+				VistaLoginAdmin vistaLoginAdmin = new VistaLoginAdmin();
+
+				String nickname = vistaRegistroAdmin.getNickname();
+				// JPasswordField devuelve un array de chars por lo que hay que tratarlo
+				// diferente
+				char[] arrayContrasenya = passwordField.getPassword();
+				String contrasenya = new String(arrayContrasenya); // Lo pasamos a String
+
+				if (nickname.equals(textField.getText()) && contrasenya.equals(vistaRegistroAdmin.getContrasenya())) {
+
+					frame3.setVisible(false);
+					VistaRecursos vistaRecursos = new VistaRecursos();
+				} else {
+					JOptionPane.showMessageDialog(frame3, "Nickname o contraseña incorrectos");
+				}
+			}
+
+		});
+
 		frame3.setVisible(true);
 	}
 
